92d1d379eff43afefe542d740443a7986ed179bf
Nitin S <atomic.unit@gmail.com>
2010-10-01 12:14:14 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimesyncTest::Application.config.secret_token = '397598bfa1301d6dc077ae7bdd6c5c736733bb479507d19fd110d451ef77b04b526144578ac822fb6f555be20f825cc50d0ff65c928e60d0b6db79a3a42653ec'
